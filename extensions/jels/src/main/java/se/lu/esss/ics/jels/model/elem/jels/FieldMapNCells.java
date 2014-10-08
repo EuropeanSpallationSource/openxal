58b42f31bb613ab9e406e72f55f0d99e47d0739d
Ivo List <ivo.list@cosylab.com>
2014-10-08 09:11:12 +0200

    OXAL-421 dynamically calculating centers of gaps in the cells (ncells for fieldmaps)

--- extensions/jels/src/main/java/se/lu/esss/ics/jels/model/elem/jels/FieldMapNCells.java
+++ extensions/jels/src/main/java/se/lu/esss/ics/jels/model/elem/jels/FieldMapNCells.java
@@ -3,9 +3,13 @@ package se.lu.esss.ics.jels.model.elem.jels;
 import se.lu.esss.ics.jels.model.elem.els.IdealDrift;
 import se.lu.esss.ics.jels.smf.impl.ESSFieldMap;
 import se.lu.esss.ics.jels.tools.math.TTFIntegrator;
+import xal.model.IComponent;
+import xal.model.IElement;
 import xal.model.IProbe;
+import xal.model.ModelException;
 import xal.model.elem.ElementSeq;
 import xal.sim.scenario.LatticeElement;
+import xal.tools.beam.PhaseMap;
 
 /**
  * Implementation of NCells simulation of fieldmaps.
@@ -17,6 +21,10 @@ import xal.sim.scenario.LatticeElement;
 public class FieldMapNCells extends ElementSeq {
 	private IdealDrift[] drifts;
 	private IdealRfGap[] gaps;
+	private TTFIntegrator[] splitIntgrs;
+	private double phi0 = 0.;
+	private double phase;
+	private double frequency;
 	
 	public FieldMapNCells() {
         this(null);
@@ -32,7 +40,7 @@ public class FieldMapNCells extends ElementSeq {
 	    final ESSFieldMap fm = (ESSFieldMap)latticeElement.getNode();
 	    final TTFIntegrator intgr = TTFIntegrator.getInstance(fm.getFieldMapFile()+".edz", fm.getFrequency()*1e6);
 	    
-	    TTFIntegrator[] splitIntgrs = intgr.getSplitIntegrators();
+	    splitIntgrs = intgr.getSplitIntegrators();
 	    
 	    /*
 	     * Old implementation of IdealRfGap is used. First gap phase is calculated when the energy at the
@@ -40,30 +48,31 @@ public class FieldMapNCells extends ElementSeq {
 	     */
 	    gaps = new IdealRfGap[splitIntgrs.length];
 	    drifts = new IdealDrift[splitIntgrs.length*2];
+	    frequency = fm.getFrequency()*1e6;
+	    phase = fm.getPhase();
 	    
-	    double beta, phi0 = 0.;
+	    double beta;
 		if (fm.getFieldMapFile().endsWith("spokeFieldMap")) beta = 0.5;
     	else if (fm.getFieldMapFile().endsWith("medBetaFieldMap")) { beta = 0.68; phi0 = Math.PI; }
     	else beta = 0.87;
 	    
 	    for (int i=0; i<splitIntgrs.length; i++) {
 	    	//final double l1 = splitIntgrs[i].getCenter();
-	    	final double l1 = splitIntgrs[i].getSyncCenter(beta, phi0);
-	    	
+	    	final double l1 = splitIntgrs[i].getSyncCenter(beta, phi0);	    	
 	    	double l2 = splitIntgrs[i].getLength() - l1;
 	    	
 	    	drifts[2*i] = new IdealDrift();
 	    	drifts[2*i].setId(fm.getId()+":DR"+2*i);
 			drifts[2*i].setLength(l1);
 						
-		    gaps[i] = new IdealRfGap(fm.getId(), splitIntgrs[i].getE0TL()*fm.getXelmax(),0, fm.getFrequency()*1e6) {
+		    gaps[i] = new IdealRfGap(fm.getId(), splitIntgrs[i].getE0TL()*fm.getXelmax(),0, fm.getFrequency()*1e6);/* {
 		    	@Override
 		    	public void calculatePhase(IProbe probe)
 		    	{    		
 		    		double dphi = 2*Math.PI*getFrequency()*l1/probe.getBeta()/LightSpeed;
 		    		setPhase(fm.getPhase()*Math.PI/180. + dphi);
 		    	}
-		    };
+		    };*/
 			
 			gaps[i].setTTFFit(splitIntgrs[i].integratorWithOffset(l1,i*Math.PI));
 			gaps[i].setFirstGap(i==0);
@@ -80,5 +89,24 @@ public class FieldMapNCells extends ElementSeq {
 			addChild(drifts[2*i+1]);	
 	    }
 	}
+	
+	
+	public void propagate(IProbe probe) throws ModelException {
+		for (int i=0; i<splitIntgrs.length; i++) {
+			double l1 = splitIntgrs[i].getSyncCenter(probe.getBeta(), phi0);
+			double l2 = splitIntgrs[i].getLength() - l1;
+			if (i==0) {
+				double dphi = 2*Math.PI*frequency*l1/probe.getBeta()/IElement.LightSpeed;
+    			gaps[i].setPhase(phase*Math.PI/180. + dphi);
+			}
+			drifts[2*i].setLength(l1);
+			drifts[2*i+1].setLength(l2);
+			gaps[i].setTTFFit(splitIntgrs[i].integratorWithOffset(l1,i*Math.PI));
+			
+			drifts[2*i].propagate(probe);
+			gaps[i].propagate(probe);
+			drifts[2*i+1].propagate(probe);
+        }
+    }
 
 }
