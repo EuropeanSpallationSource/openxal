74bb5635380bcf1cfe2287531eb0db65f4c08143
Ivo List <ivo.list@cosylab.com>
2015-02-18 14:10:44 +0100

    OXAL-421 setting position of ncells

--- extensions/jels/src/main/java/se/lu/esss/ics/jels/model/elem/jels/FieldMapNCells.java
+++ extensions/jels/src/main/java/se/lu/esss/ics/jels/model/elem/jels/FieldMapNCells.java
@@ -21,8 +21,9 @@ public class FieldMapNCells extends ElementSeq {
 	private IdealRfGap[] gaps;
 	private TTFIntegrator[] splitIntgrs;
 	private double phi0 = 0.;
-	private double phase;
+	private double phiInput;
 	private double frequency;
+	private double startPos;
 	
 	public FieldMapNCells() {
         this(null);
@@ -47,33 +48,36 @@ public class FieldMapNCells extends ElementSeq {
 	    gaps = new IdealRfGap[splitIntgrs.length];
 	    drifts = new IdealDrift[splitIntgrs.length*2];
 	    frequency = fm.getFrequency()*1e6;
-	    phase = fm.getPhase()*Math.PI/180.;
-	    
+	    phiInput = fm.getPhase()*Math.PI/180.;
+	    startPos = latticeElement.getStartPosition();
+	    		
 	    double beta;
 		if (fm.getFieldMapFile().endsWith("Spoke_F2F")) { beta = 0.5; }
     	else if (fm.getFieldMapFile().endsWith("MB_F2F")) { beta = 0.68; phi0 = Math.PI; }
     	else beta = 0.87;
 	    
 	    for (int i=0; i<splitIntgrs.length; i++) {
-	    	//final double l1 = splitIntgrs[i].getCenter();
-	    	final double l1 = splitIntgrs[i].getSyncCenter(beta, phi0);	    	
+	    	final double l1 = splitIntgrs[i].getLength() / 2.;	    	
 	    	double l2 = splitIntgrs[i].getLength() - l1;
 	    	
 	    	drifts[2*i] = new IdealDrift();
 	    	drifts[2*i].setId(fm.getId()+":DR"+2*i);
 			drifts[2*i].setLength(l1);
+			drifts[2*i].setPosition(startPos + l1/2.);
 						
-		    gaps[i] = new IdealRfGap(fm.getId(), splitIntgrs[i].getE0TL()*fm.getXelmax(),0, fm.getFrequency()*1e6);
+		    gaps[i] = new IdealRfGap(fm.getId(), splitIntgrs[i].getE0TL()*fm.getXelmax(), 0, fm.getFrequency()*1e6);
 			
 			gaps[i].setTTFFit(splitIntgrs[i].integratorWithOffset(0.));
 			gaps[i].setFirstGap(i==0);
 			gaps[i].setCellLength(fm.getLength());
 			gaps[i].setE0(fm.getXelmax());
 			gaps[i].setStructureMode(0);
+			gaps[i].setPosition(startPos + l1);
 			
 			drifts[2*i+1] = new IdealDrift();
 			drifts[2*i+1].setId(fm.getId()+":DR"+(2*i+1));
 			drifts[2*i+1].setLength(l2);	
+			drifts[2*i+1].setPosition(startPos + l1 + l2/2.);
 			
 			addChild(drifts[2*i]);
 			addChild(gaps[i]);
@@ -83,25 +87,29 @@ public class FieldMapNCells extends ElementSeq {
 	
 	
 	public void propagate(IProbe probe) throws ModelException {
-		double phase = this.phase;
+		double phi = this.phiInput;
 		for (int i=0; i<splitIntgrs.length; i++) {			
-			double phis = splitIntgrs[i].getSyncPhase(phase, probe.getBeta()) + i*Math.PI + phi0;
-			double phil = Math.IEEEremainder(phis - phase, 2*Math.PI);
-			if (phil < 0) phil+=2*Math.PI;
-			double l1 =  phil / (2*Math.PI*frequency) * probe.getBeta() * IElement.LightSpeed;		
+			double phis = splitIntgrs[i].getSyncPhase(phi, probe.getBeta()) + i*Math.PI + phi0;
+			double phil = Math.IEEEremainder(phis - phi, 2*Math.PI);
+			if (phil < 0) phil += 2*Math.PI;
+			double l1 =  phil * probe.getBeta() * IElement.LightSpeed / (2*Math.PI*frequency);		
 			double l2 = splitIntgrs[i].getLength() - l1;
 			if (i==0) {
     			gaps[i].setPhase(phis);
 			}
+			gaps[i].setTTFFit(splitIntgrs[i].integratorWithOffset(phi - phis));
+			
 			drifts[2*i].setLength(l1);
+			drifts[2*i].setPosition(startPos + l1/2.);
+			gaps[i].setPosition(startPos+l1);
 			drifts[2*i+1].setLength(l2);
-			gaps[i].setTTFFit(splitIntgrs[i].integratorWithOffset(phase - phis));
+			drifts[2*i+1].setPosition(startPos + l1 + l2/2.);
 			
 			drifts[2*i].propagate(probe);
 			gaps[i].propagate(probe);
 			drifts[2*i+1].propagate(probe);
 			
-			phase = probe.getLastGapPhase() + 2*Math.PI*frequency*l2/(probe.getBeta()*IElement.LightSpeed);
+			phi = probe.getLastGapPhase() + 2*Math.PI*frequency*l2/(probe.getBeta()*IElement.LightSpeed);
         }
     }
 
