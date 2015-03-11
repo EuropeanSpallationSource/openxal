09e984227da0a754c24ccbad8861fb3fd5012f82
Ivo List <ivo.list@cosylab.com>
2015-03-11 14:06:53 +0100

    OXAL-456 fixes to packaging lego2openxal

--- apps/packaging/esstools/src/main/scripts/lego2openxal.sh
+++ apps/packaging/esstools/src/main/scripts/lego2openxal.sh
@@ -3,4 +3,4 @@ CURRENT_DIR=`dirname $0`
 LINKTARGET=`readlink -f $CURRENT_DIR/lego2openxal`
 DIR=`dirname $LINKTARGET`
 cd $DIR/../lib/openxal && 
-java $1 -cp ":*" org.openepics.discs.exporters.OpenXALExporter
+java -cp ":*" org.openepics.discs.exporters.OpenXALExporter "$@"
