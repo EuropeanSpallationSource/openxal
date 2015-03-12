1daf652878aa08149d2d585a201f572c8e6c03d0
Ivo List <ivo.list@cosylab.com>
2015-03-12 09:56:14 +0100

    OXAL-456 adding lego2tracewin script

---
+++ apps/packaging/esstools/src/main/scripts/lego2tracewin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+CURRENT_DIR=`dirname $0`
+LINKTARGET=`readlink -f $CURRENT_DIR/lego2openxal`
+DIR=`dirname $LINKTARGET`
+cd $DIR/../lib/openxal && 
+java -cp ":*" org.openepics.discs.exporters.TraceWinExporter "$@"
