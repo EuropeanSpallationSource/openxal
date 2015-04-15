e33e2c577e7ea3081c077ec3c349b8eed4b3d151
Greg White <greg@slac.stanford.edu>
2015-04-15 16:13:13 +0200

    Changed PVA_ADDR_LIST to point to icsserv02 as new home of daemons

--- script/envs_epics_prod.bash
+++ script/envs_epics_prod.bash
@@ -1,16 +1,21 @@
 #-*-bash-*-
 #
 # envs_epics_prop.bash sets runtime EPICS CA and PVA network confirguration 
-# parameters for the production ESS EPICS controls network.
+# parameters for the production ESS EPICS controls network specifically
+# with respect to the MEME system of EPICS V4 services.
+#
+# It's expected that the functional contents of this file would be
+# copied to a general epics setup file for all host side clients in ESS.
 #
 # -------------------------------------------------------
-# Auth: Greg White, 9-Apr-2015
-# Mod:
+# Auth:  9-Apr-2015, Greg White
+# Mod:  15-Apr-2015, Greg White, changed EPICS_PVA_ADDR_LIST from physics01
+#                    to icsserv02 - the new home for daemons etc
 # =======================================================
 
 # Set gateways and services
 # export EPICS_CA_ADDR_LIST=<ESS-EPICS-GATEWAY-ADDRESS>
-export EPICS_PVA_ADDR_LIST=physics01.esss.lu.se
+export EPICS_PVA_ADDR_LIST=icsserv02.esss.lu.se
 
 # Set ESS production EPICS ports.
 export EPICS_CA_REPEATER_PORT=5065
