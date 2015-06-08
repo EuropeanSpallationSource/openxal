e96656c78d3f56220a9a14dfcdbd6893bef894e9
Ivo List <ivo.list@cosylab.com>
2015-06-08 11:04:52 +0200

    OXAL-463 workaround for probe instantiation in LinacLego converter

--- extensions/linaclego/src/se/lu/esss/linaclego/LinacLego.java
+++ extensions/linaclego/src/se/lu/esss/linaclego/LinacLego.java
@@ -20,10 +20,13 @@ import se.lu.esss.ics.jels.model.elem.jels.JElsElementMapping;
 import se.lu.esss.linaclego.elements.Drift;
 import se.lu.esss.linaclego.elements.Quad;
 import xal.model.ModelException;
+import xal.model.alg.EnvelopeTracker;
+import xal.model.alg.Tracker;
 import xal.model.probe.EnvelopeProbe;
 import xal.sim.scenario.ElementMapping;
 import xal.smf.Accelerator;
 import xal.smf.AcceleratorSeq;
+import xal.tools.beam.Twiss;
 
 public class LinacLego {
 	public static Linac load(String sourceFileName) throws JAXBException, SAXException, ParserConfigurationException, MalformedURLException 
@@ -72,10 +75,54 @@ public class LinacLego {
 		if (args.length > 1) {
 			probe = JElsDemo.loadProbeFromXML(args[1]);
 		} else {
-			probe = JElsDemo.defaultProbe();
+			probe = defaultProbe();
 		}
 		
 		JElsDemo.run(sequence, probe);
 	}
 	
+	
+	// TODO remove hardcoded probe initialisation
+	public static EnvelopeProbe defaultProbe() {
+		EnvelopeProbe probe = setupOpenXALProbe(); // OpenXAL probe & algorithm
+		//EnvelopeProbe probe = setupElsProbe(); // ELS probe & algorithm
+						
+		// Setup of initial parameters
+		setupInitialParameters(probe);
+        //loadInitialParameters(probe, "mebt-initial-state.xml");		
+		
+		return probe;
+	}
+	
+	private static EnvelopeProbe setupOpenXALProbe() {
+		EnvelopeTracker envelopeTracker = new EnvelopeTracker();			
+		envelopeTracker.setRfGapPhaseCalculation(true);
+		envelopeTracker.setUseSpacecharge(true);
+		envelopeTracker.setEmittanceGrowth(false);
+		envelopeTracker.setStepSize(0.01);
+		envelopeTracker.setProbeUpdatePolicy(Tracker.UPDATE_ALWAYS);
+		
+		EnvelopeProbe envelopeProbe = new EnvelopeProbe();
+		envelopeProbe.setAlgorithm(envelopeTracker);		
+		
+		return envelopeProbe;
+	}
+	
+	public static void setupInitialParameters(EnvelopeProbe probe) {
+		probe.setSpeciesCharge(1);
+		probe.setSpeciesRestEnergy(9.3827202900E8);
+		//elsProbe.setSpeciesRestEnergy(9.38272013e8);	
+		probe.setKineticEnergy(3.6218151e6);//energy
+		probe.setPosition(0.0);
+		probe.setTime(0.0);		
+				
+		double beta_gamma = probe.getBeta() * probe.getGamma();
+	
+		
+		probe.initFromTwiss(new Twiss[]{new Twiss(-0.051952048,0.20962859,0.2529362*1e-6 / beta_gamma),
+										  new Twiss(-0.31155119,0.37226081,0.2510271*1e-6 / beta_gamma),
+										  new Twiss(-0.48513031,0.92578192,0.3599253*1e-6 / beta_gamma)});
+		probe.setBeamCurrent(62.5e-3);
+		probe.setBunchFrequency(352.21e6); 	
+	}
 }
