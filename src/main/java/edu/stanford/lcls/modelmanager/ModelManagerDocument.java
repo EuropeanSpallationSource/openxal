caf82da778e1b0f57caca05dbcc1848b566dd464
Ivo List <ivo.list@cosylab.com>
2015-07-27 09:07:35 +0200

    Changed link to documentation

--- src/main/java/edu/stanford/lcls/modelmanager/ModelManagerDocument.java
+++ src/main/java/edu/stanford/lcls/modelmanager/ModelManagerDocument.java
@@ -158,7 +158,7 @@ public class ModelManagerDocument extends AcceleratorDocument {
 				// launch a web browser and use confluence URL as the help contents
 //            	System.out.println("launching firefox...");
         		try {
-					Desktop.getDesktop().browse(new URI("https://confluence.slac.stanford.edu/display/LCLSHELP/Model+Manager#ModelManager-Introduction"));
+					Desktop.getDesktop().browse(new URI("https://ess-ics.atlassian.net/wiki/display/SCA/Model+manager"));
 				} catch (IOException e) {
 					e.printStackTrace();
 				} catch (URISyntaxException e) {
