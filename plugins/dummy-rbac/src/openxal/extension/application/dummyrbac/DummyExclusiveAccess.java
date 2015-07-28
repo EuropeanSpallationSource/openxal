85b0d02fa7bb25feeb2d3861e7ca4d323be34cf9
Blaz Kranjc <blaz.kranjc@cosylab.com>
2015-07-28 10:01:03 +0200

    Fixed poms and date argument.

--- plugins/dummy-rbac/src/openxal/extension/application/dummyrbac/DummyExclusiveAccess.java
+++ plugins/dummy-rbac/src/openxal/extension/application/dummyrbac/DummyExclusiveAccess.java
@@ -1,13 +1,15 @@
 package openxal.extension.application.dummyrbac;
 
+import java.sql.Date;
+
 import xal.extension.application.rbac.AccessDeniedException;
 import xal.extension.application.rbac.ExclusiveAccess;
 import xal.extension.application.rbac.RBACException;
 
 public class DummyExclusiveAccess extends ExclusiveAccess{
 
-    protected DummyExclusiveAccess(String resource, String permission, long expirationDate) {
-        super(resource, permission, expirationDate);
+    protected DummyExclusiveAccess(String resource, String permission, int expirationDate) {
+        super(resource, permission, new Date(expirationDate * 60 * 1000 + System.currentTimeMillis()));
     }
 
     @Override
