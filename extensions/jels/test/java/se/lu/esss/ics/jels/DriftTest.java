5d0c68586dfb0ca2449381424549c02424871a1f
Ivo List <ivo.list@cosylab.com>
2015-08-06 10:27:50 +0200

    fixing tolerances on drift test

--- extensions/jels/test/java/se/lu/esss/ics/jels/DriftTest.java
+++ extensions/jels/test/java/se/lu/esss/ics/jels/DriftTest.java
@@ -19,13 +19,13 @@ public class DriftTest extends SingleElementTest {
 	}
 
 	
-	@Parameters
+	@Parameters(name = "Drift {index}: {0}")
 	public static Collection<Object[]> tests() {
 		final double frequency = 4.025e8, current = 0;
 		
 		List<Object []> tests = new ArrayList<>();
 		
-		// basic test, E=3MeV		
+		// 0: basic test, E=3MeV		
 		tests.add(new Object[] {new SingleElementTestData() {{
 			probe = setupOpenXALProbe( 3e6, frequency, current); 
 			elementMapping = JElsElementMapping.getInstance();
@@ -58,7 +58,7 @@ public class DriftTest extends SingleElementTest {
 			elsBeta = new double[] {3.158031E-01, 4.841974E-01, 9.758203E-01};			
 		}}});
 		
-		// high energy test, E=2.5GeV		
+		// 1: high energy test, E=2.5GeV		
 		tests.add(new Object[] {new SingleElementTestData() {{
 				probe = setupOpenXALProbe( 2.5e9, frequency, current); 
 				elementMapping = JElsElementMapping.getInstance();
@@ -87,7 +87,7 @@ public class DriftTest extends SingleElementTest {
 				
 			}}});
 
-		// space charge test, I=30mA		
+		// 2: space charge test, I=30mA		
 		tests.add(new Object[] {new SingleElementTestData() {{
 				probe = setupOpenXALProbe2( 3e6, frequency, 30e-3); 
 				elementMapping = JElsElementMapping.getInstance();
@@ -112,11 +112,12 @@ public class DriftTest extends SingleElementTest {
 						{+0.000000e+00, +0.000000e+00, +2.336272e-06, +9.191618e-06, +0.000000e+00, +0.000000e+00}, 
 						{+0.000000e+00, +0.000000e+00, +0.000000e+00, +0.000000e+00, +3.578086e-06, +3.498884e-06}, 
 						{+0.000000e+00, +0.000000e+00, +0.000000e+00, +0.000000e+00, +3.498884e-06, +6.968244e-06}};
-
+				
+				CMerrTolerance = 1e-2;
 			}}});
 		
 
-		// space charge test, I=30mA, L = 500m
+		// 3: space charge test, I=30mA, L = 500m
 		tests.add(new Object[] {new SingleElementTestData() {{
 				probe = setupOpenXALProbe2( 3e6, frequency, 30e-3); 
 				elementMapping = JElsElementMapping.getInstance();
@@ -142,9 +143,11 @@ public class DriftTest extends SingleElementTest {
 						{+0.000000e+00, +0.000000e+00, +0.000000e+00, +0.000000e+00, +4.830985e+00, +9.731960e-03}, 
 						{+0.000000e+00, +0.000000e+00, +0.000000e+00, +0.000000e+00, +9.731960e-03, +1.960492e-05}, 
 				};
+				
+				CMerrTolerance = 1e-3;
 			}}});
 
-		// space charge test, I=30mA, L = 500m, E=20GeV
+		// 4: space charge test, I=30mA, L = 500m, E=20GeV
 		tests.add(new Object[] {new SingleElementTestData() {{
 				probe = setupOpenXALProbe2( 20.e9, frequency, 30e-3); 
 				elementMapping = JElsElementMapping.getInstance();
@@ -170,6 +173,8 @@ public class DriftTest extends SingleElementTest {
 						{+0.000000e+00, +0.000000e+00, +0.000000e+00, +0.000000e+00, +1.090264e-05, +1.085451e-05}, 
 						{+0.000000e+00, +0.000000e+00, +0.000000e+00, +0.000000e+00, +1.085451e-05, +1.080661e-05}, 
 				};
+				
+				CMerrTolerance = 1e-2;
 			}}});
 
 		
