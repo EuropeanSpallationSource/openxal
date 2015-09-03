a35fb68ebb3418448db687a40116e088eea33d90
Blaz Kranjc <blaz.kranjc@cosylab.com>
2015-09-03 07:54:19 -0700

    Added javadoc to new classes.

--- src/main/java/edu/stanford/lcls/modelmanager/util/AcceleratorLoader.java
+++ src/main/java/edu/stanford/lcls/modelmanager/util/AcceleratorLoader.java
@@ -8,19 +8,21 @@ import xal.smf.Accelerator;
 import xal.smf.data.XMLDataManager;
 
 
-/* Loader of Accelerator from file for different Accelerator types.
+/**
+ * Loader of Accelerator from file for different Accelerator types.
  *
+ * @version 0.1 3 Sep 2015
  * @author Blaz Kranjc
  */
 public enum AcceleratorLoader {
-	/* Loader for  OpenXal accelerator files*/
+	// Loader for  OpenXal accelerator files. 
 	OPEN_XAL("xal") {
 		@Override
 		public Accelerator loadAccelerator(String url) {
 		    return XMLDataManager.acceleratorWithUrlSpec(new File(url).toURI().toString());
 		}
 	},
-	/* Loader for  LinacLego accelerator files*/
+	// Loader for  LinacLego accelerator files.
 	LINAC_LEGO("xml") {
 		@Override
 		public Accelerator loadAccelerator(String url) {
@@ -42,12 +44,31 @@ public enum AcceleratorLoader {
 	private String suffix;
 	private static final int size = AcceleratorLoader.values().length;
 
+	/**
+	 * Class constructor.
+	 * 
+	 * @param suffix Suffix of file to load.
+	 */
 	private AcceleratorLoader(String suffix) {
 		this.suffix = suffix;
 	}
-	
+
+	/**
+	 * Method dedicated to loading of Accelerator from file.
+         * Every loader must implement this method.
+	 * 
+	 * @param url Url of accelerator file.
+	 * @return Loaded Accelerator object.
+	 */
 	protected abstract Accelerator loadAccelerator(String url);
 
+	/**
+         * Accelerator serving method. Loads the accelerator from file
+	 * and injects missing default tables.
+	 * 
+	 * @param url Url of accelerator file.
+	 * @return Loaded Accelerator object.
+	 */
 	public Accelerator getAccelerator(String url) {
             Accelerator acc = loadAccelerator(url);
             DefaultTableLoader tableLoader = new DefaultTableLoader();
@@ -55,6 +76,12 @@ public enum AcceleratorLoader {
 	    return acc;
 	}
 
+	/**
+         * Method that serves the correct AcceleratorLoader implementation.
+	 * 
+	 * @param suffix Suffix of file to load.
+	 * @return Concrete loader.
+	 */
 	public static AcceleratorLoader findAcceleratorBySuffix(String suffix) throws UnsupportedOperationException {
 		for (AcceleratorLoader t : values()) {
 			if (t.suffix.equals(suffix)) {
@@ -64,6 +91,11 @@ public enum AcceleratorLoader {
 		throw new UnsupportedOperationException("No accelerator loader for type " + suffix + ".");
 	}
 
+	/**
+         * Provides all suffixes for which loaders are implemented.
+	 * 
+	 * @return Array of supported suffixes.
+	 */
 	public static String [] getSupportedTypes() {
 		String[] suffixes = new String[size];
 		int i = 0;
--- src/main/java/edu/stanford/lcls/modelmanager/util/DefaultTableLoader.java
+++ src/main/java/edu/stanford/lcls/modelmanager/util/DefaultTableLoader.java
@@ -7,11 +7,21 @@ import xal.tools.data.EditContext;
 import xal.tools.data.DataTable;
 import xal.tools.xml.XmlTableIO;
 
+/**
+ * Loader of missing parameter tables. Default tables are taken 
+ * from openxal jels extension.
+ *
+ * @version 0.1 3 Sep 2015
+ * @author Blaz Kranjc
+ */
 public class DefaultTableLoader {
 	private final Collection<String> requiredTables;
 	static public final String PARAMS_FILEPATH = "edu/stanford/lcls/modelmanager/util/model.params";
 	private final EditContext defaultContext;
 
+	/**
+	 * Class Constructor
+	 */
 	public DefaultTableLoader() {
 		defaultContext = new EditContext();
 		File paramsFile = new File(getClass().getClassLoader().getResource(PARAMS_FILEPATH).getFile());
@@ -19,6 +29,11 @@ public class DefaultTableLoader {
 		requiredTables = defaultContext.getTableNames();
 	}
 
+	/**
+	 * Loads missing tables from default selection into provided context.
+	 * 
+	 * @param context Context to which tables are loaded.
+	 */
 	public void loadDefaultTables(EditContext context) {
 		for (String tableId : requiredTables) {
 			if (! (context.getTable(tableId) == null)) {
