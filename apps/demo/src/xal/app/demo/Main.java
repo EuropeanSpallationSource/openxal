532c52bee0c55983692e7b6b6b9bab4e3bc1f533
Yngve Inntjore Levinsen <Yngve.Levinsen@esss.se>
2016-03-08 15:03:23 +0100

    use applicationFinishedLaunching from abstract class

--- apps/demo/src/xal/app/demo/Main.java
+++ apps/demo/src/xal/app/demo/Main.java
@@ -198,15 +198,6 @@ public class Main extends ApplicationAdaptor {
     
     // --------- Application events --------------------------------------------
     
-    /** 
-     * Capture the application launched event and print it.  This is an optional
-     * hook that can be used to do something useful at the end of the application launch.
-     */
-    public void applicationFinishedLaunching() {
-        System.out.println("Application has finished launching!");
-		Logger.getLogger("global").log( Level.INFO, "Application has finished launching." );
-    }
-    
     
     /**
      * Constructor
