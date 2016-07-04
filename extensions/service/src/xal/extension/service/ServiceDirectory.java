754594ee1396187f6fa69eefb91c2d073aac481a
Blaz Kranjc <blaz.kranjc@cosylab.com>
2016-07-04 03:09:30 -0700

    Removed deps on jmDNS and created a skeleton.

--- extensions/service/src/xal/extension/service/ServiceDirectory.java
+++ extensions/service/src/xal/extension/service/ServiceDirectory.java
@@ -14,17 +14,9 @@ import xal.tools.coding.json.JSONCoder;
 import xal.tools.coding.*;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.jmdns.JmDNS;
-import javax.jmdns.ServiceEvent;
-import javax.jmdns.ServiceInfo;
-
 
 /**
  * ServiceDirectory is a local point of access for registering and looking up services on a network.
@@ -38,34 +30,20 @@ final public class ServiceDirectory {
 	/** The default directory */
 	static final private ServiceDirectory DEFAULT_DIRECTORY = new ServiceDirectory();
 	
-	/** thread pool */
-	final private ExecutorService THREAD_POOL;
-    
     /** coder for encoding and ecoding messages for remote transport */
     final private Coder MESSAGE_CODER;
 	
 	/** XML-RPC server used for registering services */
     private RpcServer _rpcServer;
 	
-	/** JmDNS instance */
-	private JmDNS _bonjour;
-	
 	/** ServiceDirectory constructor. */
 	private ServiceDirectory() throws ServiceException {
-		THREAD_POOL = Executors.newCachedThreadPool();
         MESSAGE_CODER = JSONCoder.getInstance();
 		
 		try {
-			try {
-				_bonjour = JmDNS.create( InetAddress.getLocalHost() );
-			}
-			catch( Exception exception ) {
-				final String message = "Error attempting to initialize JmDNS.  Will attempt to try loopback mode instead of networked mode.";
-				Logger.getLogger("global").log( Level.WARNING, message, exception );
-				System.err.println( message );
-				_bonjour = JmDNS.create( InetAddress.getByName( "127.0.0.1" ) );
-			}
-			
+		    // TODO create context and database
+		    
+		    
 			// shutdown the service directory when quitting the process
 			Runtime.getRuntime().addShutdownHook( new Thread() {
 				public void run() {
@@ -92,17 +70,11 @@ final public class ServiceDirectory {
 	}
 	
 	
-	/** Shutdown bonjour and the RPC server and dispose of all resources. */
+	/** Shutdown the pvAccess context and database and the RPC server and dispose of all resources. */
 	public void dispose() {
-		if ( _bonjour != null ) {
-            try {
-                _bonjour.close();
-                _bonjour = null;
-            }
-            catch( IOException exception ) {
-                throw new RuntimeException( "Exception closing bonjour services.", exception );
-            }
-		}
+
+		// Stop context and database TODO
+
 		if ( _rpcServer != null ) {
             try {
                 _rpcServer.shutdown();
@@ -135,10 +107,7 @@ final public class ServiceDirectory {
 	 * @return a new service reference for successful registration and null otherwise.
      */
     private <ProtocolType> ServiceRef registerService( final Class<ProtocolType> protocol, final String serviceName, final ProtocolType provider, final Map<String,Object> properties ) {
-		properties.put( ServiceRef.SERVICE_KEY, serviceName );
         
-        final String serviceType = getDefaultType( protocol );
-		
 		try {
             if ( _rpcServer == null ) {
                 _rpcServer = new RpcServer( MESSAGE_CODER );
@@ -150,11 +119,9 @@ final public class ServiceDirectory {
 			// add the service to the RPC Server
 			_rpcServer.addHandler( serviceName, protocol, provider );
 			
-			// advertise the service to the world
-			final String bonjourType = ServiceRef.getFullType( serviceType );
-			final ServiceInfo info = ServiceInfo.create( bonjourType, serviceName, port, 0, 0, properties );
-			_bonjour.registerService( info );
-			return new ServiceRef( info );
+			// advertise the service to the world TODO
+
+			return new ServiceRef(serviceName, "", port); // TODO where do I get IP?!
 		}
 		catch( Exception exception ) {
 			throw new ServiceException( exception, "Exception while attempting to register a service..." );
@@ -168,9 +135,9 @@ final public class ServiceDirectory {
 	 * @return A proxy implementing the specified protocol for the specified service reference 
 	 */
 	public <T> T getProxy( final Class<T> protocol, final ServiceRef serviceRef ) {
-        final ServiceInfo info = serviceRef.getServiceInfo();
+        final int port = serviceRef.getPort();
         final String hostAddress = serviceRef.getHostAddress();		
-		return new ClientHandler<T>( hostAddress, info.getPort(), serviceRef.getServiceName(), protocol, MESSAGE_CODER ).getProxy();
+		return new ClientHandler<T>( hostAddress, port, serviceRef.getRawName(), protocol, MESSAGE_CODER ).getProxy();
 	}
 	
 	/**
@@ -190,48 +157,7 @@ final public class ServiceDirectory {
 	 */
 	private void addServiceListener( final String type, final ServiceListener listener ) throws ServiceException {
 		try {
-			final String bonjourType = ServiceRef.getFullType( type );
-			_bonjour.addServiceListener( bonjourType, new javax.jmdns.ServiceListener() {
-				
-				/**
-				 * A service is added.
-				 * @param type the fully qualified type of the service
-				 * @param name the fully qualified name of the service
-				 */
-				public void serviceAdded( final ServiceEvent event ) {
-					System.out.println( "Service added: " + event.getName() );
-					THREAD_POOL.execute( new Runnable() {
-						public void run() {
-							event.getDNS().requestServiceInfo( event.getType(), event.getName() );		
-						}
-					});
-				}
-				
-				
-				/**
-				 * A service is removed.
-				 * @param type the fully qualified type of the service
-				 * @param name the fully qualified name of the service
-				 */
-				public void serviceRemoved( final ServiceEvent event ) {
-					System.out.println( "Service removed: " + event.getName() );
-					final String type = event.getType();
-					listener.serviceRemoved( ServiceDirectory.this, ServiceRef.getBaseType( type ), event.getName() );
-				}
-				
-				
-				/**
-				 * A service is resolved. Its details are now available in the ServiceInfo record.
-				 * @param type the fully qualified type of the service
-				 * @param name the fully qualified name of the service
-				 * @param info the service info record, or null if the service could be be resolved
-				 */
-				public void serviceResolved( final ServiceEvent event ) {
-					final ServiceInfo info  = event.getInfo();
-					final ServiceRef serviceRef = new ServiceRef( info );
-					listener.serviceAdded( ServiceDirectory.this, serviceRef );
-				}
-			});
+		    // TODO Add listener functions
 		}
 		catch(Exception exception) {
 			Logger.getLogger("global").log( Level.SEVERE, "Error attempting to add a service listener of service type: " + type, exception );
@@ -246,8 +172,7 @@ final public class ServiceDirectory {
 	 * @return A valid type to represent the given protocol.
 	 */
 	private static String getDefaultType( final Class<?> protocol ) {
-		String id = protocol.getName();
-		return id.replace('.', '_');
+		return protocol.getName();
 	}
 	
 }
--- extensions/service/src/xal/extension/service/ServiceRef.java
+++ extensions/service/src/xal/extension/service/ServiceRef.java
@@ -10,10 +10,6 @@
 
 package xal.extension.service;
 
-import javax.jmdns.ServiceInfo;
-import java.util.regex.*;
-
-
 /**
  * ServiceRef wraps the native Rendezvous ServiceInfo and provides a reference to the service
  * that hides a direct reference to Rendezvous.
@@ -22,104 +18,44 @@ import java.util.regex.*;
  * @author <a href="mailto:blaz.kranjc@cosylab.com">Blaz Kranjc</a>
  */
 public class ServiceRef {
-    /** standard local suffix for the service type */
-    private static final String LOCAL_TYPE_SUFFIX = "_tcp.local.";
-    
-	/** Pattern matching the protocol and DNS part of the type */
-	private static final Pattern TYPE_PATTERN = Pattern.compile("(\\w++)\\._tcp\\.local\\.");
-	
-	/** Property identifying the local service handler */
-	static final String SERVICE_KEY = "remote_service_handler";
-	
-	/** Redezvous service info */
-	private ServiceInfo _serviceInfo;
+    private final String serviceName;
+    private final String hostAddress;
+    private final int port;
 		
+
 	/**
 	 * Create a new service reference to wrap the specified service info.
 	 */
-	ServiceRef(ServiceInfo serviceInfo) {
-		_serviceInfo = serviceInfo;
+	ServiceRef(String serviceName, String hostAddress, int port) {
+	    this.serviceName = serviceName;
+	    this.hostAddress = hostAddress;
+	    this.port = port;
 	}
 	
-	
-	/**
-	 * Get the name of the service.
-	 * @return The name of the service provided.
-	 */
-	String getServiceName() {
-		return _serviceInfo.getPropertyString( SERVICE_KEY );
-	}
-	
-	
+
 	/**
 	 * Get the unique raw name of the service provider.
 	 * @return The raw name of the service provider.
 	 */
 	public String getRawName() {
-		return _serviceInfo.getName();
+		return serviceName; 
 	}
-	
-	
-    /** get the service address given the service info */
-    private static String getHostAddress( final ServiceInfo info ) {
-        final String[] hostAddresses = info.getHostAddresses();
-        final String hostAddress = hostAddresses != null && hostAddresses.length > 0 ? hostAddresses[0] : null;
-		return hostAddress;
-    }
-	
+
 	
 	/**
 	 * Get the address of the remote service.
 	 * @return the address of the remote service
 	 */
 	String getHostAddress() {
-        return getHostAddress( _serviceInfo );
-	}
-	
-	/**
-	 * Get the Rendezvous service info which is wrapped by this instance.
-	 * @return The wrapped Rendezvous service info.
-	 */
-	ServiceInfo getServiceInfo() {
-		return _serviceInfo;
-	}
-	
-	
-	/**
-	 * Override equals to compare the wrapped service info instances.
-	 * @param other The other service reference against which to compare this one.
-	 * @return true if the other service reference refers to the same service provider as the this instance and false otherwise.
-	 */
-	public boolean equals(Object other) {
-		return _serviceInfo.equals( ((ServiceRef)other)._serviceInfo); 
+        return hostAddress;
 	}
 
-
-	/** override hashCode as required for consistency with equals */
-	public int hashCode() {
-		return _serviceInfo.hashCode();
-	}
-	
 	
 	/**
-	 * Internally used to strip the communication protocol and DNS address from the full type name.
-	 * @param fullType The full rendezvous type (e.g. "greeting._tcp._local.")
-	 * @return Just the simple base type (e.g. "greeting")
+	 * @return port used by the remote service.
 	 */
-	static String getBaseType( final String fullType ) {
-		Matcher matcher = TYPE_PATTERN.matcher(fullType);
-		matcher.matches();
-		return matcher.group(1);
-	}
-		
-	
-	/**
-	 * Internally used to construct the full bonjour type from the simple base type.
-	 * @param baseType The simple base type (e.g. "greeting")
-	 * @return The full rendezvous type (e.g. "greeting._tcp.local.")
-	 */
-	static String getFullType( final String baseType ) {
-		return "_" + baseType.toLowerCase() + "." + LOCAL_TYPE_SUFFIX;
+	int getPort() {
+	    return port;
 	}
 }
 
