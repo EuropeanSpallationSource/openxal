e73ff80b69db253e4c86575e48cd060063727374
Juan F. Esteban Müller <juanf.estebanmuller@esss.se>
2017-08-18 14:38:11 +0200

    Using AcceleratorExporter to export all Open XAL files. Command-line options modified for that purpose.

--- extensions/tracewin/src/xal/extension/tracewinimporter/TraceWin.java
+++ extensions/tracewin/src/xal/extension/tracewinimporter/TraceWin.java
@@ -2,43 +2,44 @@ package xal.extension.tracewinimporter;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.util.List;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import eu.ess.bled.Subsystem;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import xal.extension.jels.ImporterHelpers;
+
 import xal.extension.tracewinimporter.openxalexporter.OpenXalExporter;
 import xal.extension.tracewinimporter.parser.TraceWinImporter;
 import xal.extension.jels.model.elem.jels.JElsElementMapping;
+import xal.extension.jels.smf.ESSAccelerator;
+import xal.extension.tracewinimporter.openxalexporter.AcceleratorExporter;
+
 import xal.sim.scenario.ElementMapping;
-import xal.smf.Accelerator;
-import xal.tools.xml.XmlDataAdaptor;
-import xal.tools.xml.XmlWriter;
 
 /**
  * Loader for accelerators in TraceWin formatted files.
  *
  * <p>
- * Uses Bled importer to import accelerator from traceIn to Bled format and then
- * exports it to openxalFormat.
+ * Import accelerator from TraceWin to OpenXAL format SMF.
  * </p>
  *
  * @version 0.1 4 Sep 2015
  * @author Blaz Kranjc
+ *
+ * @version 0.2 11 Jul 2017
+ * @author Juan F. Esteban Müller <juanf.estebanmuller@esss.se>
  */
 public class TraceWin {
-
     /**
      * Calls {{@link #loadAcceleator(String, ElementMapping)} with default
      * (JEls) element mapping.
@@ -49,27 +50,27 @@ public class TraceWin {
      * @return accelerator
      * @throws IOException if there was a problem reading from file
      */
-    public static Accelerator loadAcceleator(URI sourceFileName) throws IOException {
+    public static ESSAccelerator loadAcceleator(URI sourceFileName) throws IOException {
         return loadAcceleator(sourceFileName, JElsElementMapping.getInstance());
     }
 
     /**
      * Loads accelerator from given file
      *
-     * @param fileName traceWin formatted file in which the accelerator is.
+     * @param sourceFileName traceWin formatted file in which the accelerator
+     * is.
      * @param modelMapping smf mapping for accelerator nodes to be set to
      * accelerator after conversion.
      * @return accelerator
      * @throws IOException if there was a problem reading from file
      */
-    public static Accelerator loadAcceleator(URI fileName, ElementMapping modelMapping) throws IOException {
-        Accelerator acc;
+    public static ESSAccelerator loadAcceleator(URI sourceFileName, ElementMapping modelMapping) throws IOException {
+        ESSAccelerator acc;
         // Importing from TraceWin formated file
         TraceWinImporter importer = new TraceWinImporter();
-        List<Subsystem> systems;
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(fileName.toURL().openStream()))) {
-            systems = importer.importFromTraceWin(br, new PrintWriter(System.err), new File(fileName).getParentFile().toURI().toString());
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(sourceFileName.toURL().openStream()));
+        List<Subsystem> systems = importer.importFromTraceWin(br, new PrintWriter(System.err), new File(sourceFileName).getParentFile().toURI().toString());
+        br.close();
 
         // Exporting to openxal format
         OpenXalExporter exporter = new OpenXalExporter();
@@ -78,18 +79,21 @@ public class TraceWin {
         // Setting element mapping
         acc.setElementMapping(modelMapping);
 
+        // Loading hardcoded initial paramaters. 
+        // TODO: load from Tracewin init files
+        ImporterHelpers.addHardcodedInitialParameters(acc);
+
         return acc;
 
     }
 
     /**
-     * Converts accelerator in TraceWin formatted file into openxal format and
-     * writes it to destination file or standard output.
+     * Converts accelerator in TraceWin formatted file to the OpenXAL format
+     * with all all required files.
      * <p>
-     * Usage: TraceWin inputFile [outputFile] If no output file is given,
-     * accelerator will be printed to standard output in openxal format.
-     * inputFile TraceWin formatted file in which the accelerator is. (.dat)
-     * outputFile file to write accelerator in (.xdxf)
+     * Usage: TraceWin inputFile outputFile inputFile TraceWin formatted file in
+     * which the accelerator is. (.dat) outputDir directory where export Open
+     * XAL files outputName file name of the accelerator files to generate
      *
      * </p>
      *
@@ -97,54 +101,32 @@ public class TraceWin {
      */
     public static void main(String[] args) {
         // Checking commandline arguments
-        if (args.length < 1) {
-            System.out.println("Usage: TraceWin inputFile [outputFile]");
-            System.out.println("	if no output file is given, accelerator will be printed to standard output.");
+        if (args.length < 3) {
+            System.out.println("Usage: TraceWin inputFile outputDir outputName");
             System.exit(0);
         }
         final String input = args[0];
-        String output = null;
-        if (args.length > 1) {
-            output = args[1];
-        }
+        final String outputDir = args[1];
+        final String outputName = args[2];
+
         // Starting conversion
         System.out.println("Started parsing.");
-        Accelerator acc = null;
+        ESSAccelerator accelerator = null;
         try {
-            acc = loadAcceleator(new File(input).toURI());
+            accelerator = loadAcceleator(new File(input).toURI());
         } catch (IOException e1) {
             System.err.println("Error while trying to read file.");
             System.exit(1);
         }
         System.out.println("Parsing finished.");
 
-        // Choosing output destination
+        AcceleratorExporter accExp = new AcceleratorExporter(accelerator, outputDir, outputName);
         try {
-            OutputStream out;
-            if (output == null) {
-                System.out.println("Parsed accelerator: ");
-                out = System.out;
-            } else {
-                out = new FileOutputStream(output);
-                System.out.println("Writing accelerator to file.");
-            }
-            // Writing accelerator
-            OutputStreamWriter writer;
-            writer = new OutputStreamWriter(out);
-            XmlDataAdaptor da = XmlDataAdaptor.newDocumentAdaptor(acc, "xdxf.dtd");
-            Document document = da.document();
-            cleanup(document);
-            XmlWriter.writeToWriter(document, writer);
-            writer.flush();
-            writer.close();
-
-            System.out.println("Finished");
-        } catch (FileNotFoundException e) {
-            System.err.println("Error while trying to write to file.");
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Error while flushing and closing file.");
-            System.exit(1);
+            accExp.export();
+        } catch (IOException ex) {
+            Logger.getLogger(TraceWin.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(TraceWin.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -153,7 +135,7 @@ public class TraceWin {
      *
      * @param parent node to clean
      */
-    private static void cleanup(Node parent) {
+    public static void cleanup(Node parent) {
         NodeList children = parent.getChildNodes();
         NamedNodeMap attrs = parent.getAttributes();
         if (attrs != null) {
@@ -201,5 +183,7 @@ public class TraceWin {
                 }
             }
         }
+
     }
+
 }
