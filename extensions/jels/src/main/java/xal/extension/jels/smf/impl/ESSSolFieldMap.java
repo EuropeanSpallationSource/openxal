76c7bc51d65c3ec2bd653f8f29fbca3c7bb927de
Juan F. Esteban Müller <JuanF.EstebanMuller@esss.se>
2018-02-19 16:13:14 +0100

    Adding licence header to the SolFieldMap classes.

--- extensions/jels/src/main/java/xal/extension/jels/smf/impl/ESSSolFieldMap.java
+++ extensions/jels/src/main/java/xal/extension/jels/smf/impl/ESSSolFieldMap.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 European Spallation Source ERIC.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 package xal.extension.jels.smf.impl;
 
 import java.net.URI;
@@ -32,7 +49,7 @@ public class ESSSolFieldMap extends Electromagnet {
     protected ESSSolFieldMapBucket m_bucSolFieldMap;
     // radial component of the field profile  
     protected FieldProfile2D fieldProfileR;
-    // longitudianl component of the field profile
+    // longitudinal component of the field profile
     protected FieldProfile2D fieldProfileZ;
 
     static {
