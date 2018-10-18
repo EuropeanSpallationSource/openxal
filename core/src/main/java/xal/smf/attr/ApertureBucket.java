1fc4a61f380ecbdad87305e3b7c2ed6721fe5a95
Juan F. Esteban Müller <JuanF.EstebanMuller@esss.se>
2018-10-18 13:55:09 +0200

    Adding extra "pos" attribute to the Aperture bucket class, as well as changing types to double arrays. See OXAL-582.

--- core/src/main/java/xal/smf/attr/ApertureBucket.java
+++ core/src/main/java/xal/smf/attr/ApertureBucket.java
@@ -7,15 +7,25 @@
 package xal.smf.attr;
 
 /**
+ * The aperture bucket defines the inner geometry of the elements.
+ * <p>
+ * For elements with constant aperture, set a single value for x and y.
+ * <p>
+ * For elements with more complicated geometry, use the array "pos" to define
+ * the apertures "x" and "y" (also arrays of the same length) at different
+ * points in the element. The first and last points should correspond to the
+ * ends of the element.
  *
- * @author  Christopher K. Allen
+ * @author Christopher K. Allen
  * @version 1.0
+ * @author Juan F. Esteban Müller <JuanF.EstebanMuller@esss.se>
+ * @version 2.0
  */
 
 
 public class ApertureBucket extends AttributeBucket {
     /** ID for serializable version */
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 2L;
     
 
     
@@ -33,8 +43,9 @@ public class ApertureBucket extends AttributeBucket {
     public final static String  c_strType = "aperture"; 
 
     final static String[]       c_arrNames = {  "shape",
-                                                "x",
-                                                "y"
+                                                "x",    // Aperture in the horizontal plane.
+                                                "y",    // Aperture in the vertical plane.
+                                                "pos"   // Position in the element.
                                 };
     
 
@@ -46,6 +57,7 @@ public class ApertureBucket extends AttributeBucket {
     private Attribute m_attShape;
     private Attribute m_attAperX;
     private Attribute m_attAperY;
+    private Attribute m_attAperPos;
     
     
     /*
@@ -65,21 +77,29 @@ public class ApertureBucket extends AttributeBucket {
         super();
         
         m_attShape  = new Attribute(0);
-        m_attAperX  = new Attribute(0.0);
-        m_attAperY  = new Attribute(0.0);
+        m_attAperX  = new Attribute(new double[] {0.0});
+        m_attAperY  = new Attribute(new double[] {0.0});
+        m_attAperPos  = new Attribute(new double[] {0.0});
         
         super.registerAttribute(c_arrNames[0], m_attShape);
         super.registerAttribute(c_arrNames[1], m_attAperX);
         super.registerAttribute(c_arrNames[2], m_attAperY);
+        super.registerAttribute(c_arrNames[3], m_attAperPos);
     };
 
     
     public int      getShape()  { return m_attShape.getInteger(); };
-    public double   getAperX()  { return m_attAperX.getDouble(); };
-    public double   getAperY()  { return m_attAperY.getDouble(); };
+    public double[]   getAperX()  { return m_attAperX.getArrDbl(); };
+    public double[]   getAperY()  { return m_attAperY.getArrDbl(); };
+    public double[]   getAperPos()  { return m_attAperPos.getArrDbl(); };
     
     public void setShape(int intVal)    { m_attShape.set(intVal); };
-    public void setAperX(double dblVal) { m_attAperX.set(dblVal); };
-    public void setAperY(double dblVal) { m_attAperY.set(dblVal); };
+    public void setAperX(double dblVal) { m_attAperX.set(new double[] {dblVal}); };
+    public void setAperY(double dblVal) { m_attAperY.set(new double[] {dblVal}); };
+    public void setAperPos(double dblVal) { m_attAperPos.set(new double[] {dblVal}); };
+    
+    public void setAperX(double[] dblArr) { m_attAperX.set(dblArr); };
+    public void setAperY(double[] dblArr) { m_attAperY.set(dblArr); };
+    public void setAperPos(double[] dblArr) { m_attAperPos.set(dblArr); };
     
 };
