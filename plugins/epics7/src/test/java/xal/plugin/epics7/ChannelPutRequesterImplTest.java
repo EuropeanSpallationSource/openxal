08e9843ed0f0d946fe2eb4deaad110095a3670ee
Juan F. Esteban Müller <juanf.estebanmuller@ess.eu>
2020-05-26 15:48:22 +0200

    Unit test for PutListenerImpl and some fixes...

--- plugins/epics7/src/test/java/xal/plugin/epics7/ChannelPutRequesterImplTest.java
+++ plugins/epics7/src/test/java/xal/plugin/epics7/ChannelPutRequesterImplTest.java
@@ -1,7 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2020 European Spallation Source ERIC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 package xal.plugin.epics7;
 
@@ -16,10 +28,6 @@ import org.epics.pvdata.pv.PVStructure;
 import org.epics.pvdata.pv.ScalarType;
 import org.epics.pvdata.pv.Status;
 import org.epics.pvdata.pv.Structure;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import xal.ca.PutListener;
@@ -30,28 +38,9 @@ import static xal.plugin.epics7.Epics7Channel.TIMESTAMP_FIELD;
 
 /**
  *
- * @author juanfestebanmuller
+ * @author Juan F. Esteban Müller <JuanF.EstebanMuller@ess.eu>
  */
 public class ChannelPutRequesterImplTest {
-
-    public ChannelPutRequesterImplTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     boolean eventListenerCalled = false;
     boolean putListenerCalled = false;
