85ff4d0898ed33f01d47fb549dc23e538a900970
Juan F. Esteban Müller <JuanF.EstebanMuller@esss.se>
2021-06-16 14:19:10 +0200

    fixing bug in Tracker and ProbeState that was not properly storing element type

--- core/src/main/java/xal/model/alg/Tracker.java
+++ core/src/main/java/xal/model/alg/Tracker.java
@@ -743,6 +743,7 @@ public abstract class Tracker implements IAlgorithm, IArchive {
             return;
         
        probe.setCurrentElement(elem.getId());
+       probe.setCurrentElementTypeId(elem.getType());
        probe.setCurrentHardwareId(elem.getHardwareNodeId());
 //     sako *** IMPORTANT CHANGES
 //        this is now moved to Element.propagate this.setElemPosition(0.0);
