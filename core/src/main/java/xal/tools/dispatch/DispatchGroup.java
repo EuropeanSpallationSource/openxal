3ffc299b09cef5ee2d6fb11da95ecbaa8a3edf87
Juan F. Esteban Müller <JuanF.EstebanMuller@esss.se>
2021-07-02 17:25:44 +0200

    trying to fix bug in multithreading - increasing/decreasing volatile counter

--- core/src/main/java/xal/tools/dispatch/DispatchGroup.java
+++ core/src/main/java/xal/tools/dispatch/DispatchGroup.java
@@ -8,6 +8,7 @@
 package xal.tools.dispatch;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,24 +28,16 @@ public class DispatchGroup implements DispatchOperationListener {
      * count of pending operations belonging to this group which have not yet
      * completed
      */
-    private volatile int pendingOperationCount;
+    private AtomicInteger pendingOperationCount = new AtomicInteger(0);
 
     /**
      * lock for waiting on empty
      */
-    private final Object emptyWaitLock;
+    private final Object emptyWaitLock = new Object();
 
     // static initializer
     static {
         LOCAL_CURRENT_GROUPS = new LocalGroups();
-    }
-
-    /**
-     * Constructor
-     */
-    public DispatchGroup() {
-        emptyWaitLock = new Object();
-        pendingOperationCount = 0;
     }
 
     /**
@@ -82,7 +75,7 @@ public class DispatchGroup implements DispatchOperationListener {
     public void waitForCompletion() {
         // while loop protects against accidental wake since wait is not guaranteed
         synchronized (emptyWaitLock) {
-            while (pendingOperationCount > 0) {
+            while (pendingOperationCount.get() > 0) {
                 try {
                     emptyWaitLock.wait();
                 } catch (InterruptedException exception) {
@@ -108,15 +101,19 @@ public class DispatchGroup implements DispatchOperationListener {
         // maximum time until expiration
         final long maxTime = new Date().getTime() + timeout;
         // while loop protects against accidental wake since wait is not guaranteed
-        while (pendingOperationCount > 0 && new Date().getTime() < maxTime) {
+        while (new Date().getTime() < maxTime) {
             final long remainingTime = Math.max(0, maxTime - new Date().getTime());
             // remaining time must be strictly greater than zero otherwise the wait will wait forever until notified
-            if (remainingTime > 0) {
-                try {
-                    synchronized (emptyWaitLock) {
+            synchronized (emptyWaitLock) {
+                if (pendingOperationCount.get() <= 0) {
+                    break;
+                }
+                if (remainingTime > 0) {
+                    try {
                         emptyWaitLock.wait(remainingTime);
+                    } catch (InterruptedException exception) {
+                        LOGGER.log(Level.WARNING, null, exception);
                     }
-                } catch (InterruptedException exception) {
                 }
             }
         }
@@ -155,24 +152,22 @@ public class DispatchGroup implements DispatchOperationListener {
      * increment the pending operation count
      */
     private void incrementPendingOperationCount() {
-        synchronized (emptyWaitLock) {
-            pendingOperationCount++;
-        }
+        pendingOperationCount.incrementAndGet();
     }
 
     /**
      * increment the pending operation count
      */
-    synchronized private void decrementPendingOperationCount() {
-        synchronized (emptyWaitLock) {
-            pendingOperationCount--;
-            try {
-                if (pendingOperationCount == 0) {
+    private void decrementPendingOperationCount() {
+        pendingOperationCount.decrementAndGet();
+        try {
+            synchronized (emptyWaitLock) {
+                if (pendingOperationCount.get() == 0) {
                     emptyWaitLock.notifyAll();
                 }
-            } catch (Exception exception) {
-                LOGGER.log(Level.SEVERE, "Failed attempt to awake threads waiting on this group.", exception);
             }
+        } catch (Exception exception) {
+            LOGGER.log(Level.SEVERE, "Failed attempt to awake threads waiting on this group.", exception);
         }
     }
 
